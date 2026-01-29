ee0d737f85828a6fc26f8ddce93be77f8af0714e
Eyassu Betru <eyassubetru0@gmaol.com>
2026-01-29 18:03:57 +0300

    origin: "http://localhost:5173", // your frontend URL   credentials: true

--- src/app.js
+++ src/app.js
@@ -16,16 +16,7 @@ const allowedOrigins = [
 
 const app = express();
 app.use(cookieParser());
-app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+app.use(cors({origin: "http://localhost:5173", // your frontend URL
   credentials: true
 }));
 app.use(express.json({ limit: '500mb' }));
