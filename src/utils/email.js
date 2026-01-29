97f8a859054cc20cedf7c04503d4eefb782a2251
Eyassu Betru <eyassubetru0@gmaol.com>
2026-01-29 17:23:18 +0300

    secure:false

--- src/utils/email.js
+++ src/utils/email.js
@@ -5,6 +5,7 @@ export const sendEmail = async (to, subject, text, html) => {
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
+      secure:false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
